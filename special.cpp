368fca1aec7d2a2ba646dd6442021c76d4c4e796
SIDDHANT THAKUR <siddhant.76333@purc.tu.edu.np>
2021-08-17 16:47:37 +0545

    Unit Converter updated

--- special.cpp
+++ special.cpp
@@ -7,8 +7,6 @@ Special::Special(QWidget *parent) :
 {
     ui->setupUi(this);
     connect(ui->pbuc,SIGNAL(released()),this,SLOT(on_pushButton_1_released()));
-
-
 }
 
 Special::~Special()
@@ -23,8 +21,7 @@ void Special::on_pushButton_1_released()
 
 void Special::on_pushButton_5_released()//for universal constants
 {
-    Constants = new constants(this);
-    Constants->show();
+    Special::close();//to close current box
 
 }
 
